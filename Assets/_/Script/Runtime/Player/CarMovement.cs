85fe9cde87518254d0d9eb2ff046cb4ca948e514
Vincent <v1996.vincent@gmail.com>
2026-05-26 15:36:07 +0200

    fix(script)
    - correction movement player

--- Assets/_/Script/Runtime/Player/CarMovement.cs
+++ Assets/_/Script/Runtime/Player/CarMovement.cs
@@ -14,6 +14,14 @@ public class CarMovement : MonoBehaviour
     public float _maxSpeed = 12f;
     public float _rotationSpeed = 130f;
 
+    [Space(2), Header("Feel")]
+    [Range(0f, 1f)]
+    [Tooltip("1 = glisse lat?rale, 0 = la voiture suit son forward.")]
+    public float _driftFactor = 0.15f;
+
+    [Tooltip("Frein naturel sans gaz (0-1).")]
+    public float _decelerationDrag = 0.94f;
+
     public Vector2 _moveInput { get; private set; }
 
     public static event Action OnPlayerSelectedStart;
@@ -35,24 +43,33 @@ public class CarMovement : MonoBehaviour
 
         m_actions = new InputSystem_Actions();
         m_actions.Enable();
+        
     }
 
     private void Update()
     {
-        if (_isRacing) return;
+        if (_isRacing)
+        {
+            return;
+        }
 
         if (m_actions.Player.Jump.WasPressedThisFrame())
             OnPlayerSelectedStart?.Invoke();
+
     }
 
     private void FixedUpdate()
     {
-        if (!_isRacing) return;
+        if (!_isRacing) 
+        {
+            ApplyMovement(0f);
+            return; 
+        }
 
         _moveInput = m_actions.Player.Move.ReadValue<Vector2>();
 
         ApplyTurn(_moveInput.x);
-        ApplyMovement(_moveInput.y);
+        ApplyMovement(1f);
     }
 
     private void OnDestroy()
@@ -66,12 +83,17 @@ public class CarMovement : MonoBehaviour
 
     private void ApplyMovement(float move)
     {
-        if (Mathf.Approximately(move, 0f)) return;
-
-        m_rb.AddForce(transform.forward * move * _acceleration, ForceMode.Acceleration);
+        if (!Mathf.Approximately(move, 0f))
+            m_rb.AddForce(transform.forward * move * _acceleration, ForceMode.Acceleration);
 
         if (m_rb.linearVelocity.magnitude > _maxSpeed)
             m_rb.linearVelocity = m_rb.linearVelocity.normalized * _maxSpeed;
+
+        float lateralSpeed = Vector3.Dot(m_rb.linearVelocity, transform.right);
+        m_rb.linearVelocity -= transform.right * lateralSpeed * (1f - _driftFactor);
+
+        if (Mathf.Approximately(move, 0f))
+            m_rb.linearVelocity *= _decelerationDrag;
     }
 
     private void ApplyTurn(float turn)
@@ -88,12 +110,14 @@ public class CarMovement : MonoBehaviour
         }
     }
 
+    
+
     #endregion
 
     #region Private & Protected
 
     private Rigidbody m_rb;
     private InputSystem_Actions m_actions;
-
+  
     #endregion
 }
